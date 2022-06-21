e8ab23e9106b7c11599ed8c821350adff8804ec5
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-06-21 18:54:07 -0500

    Fixed exp dynamic color in safari browsers

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -1,16 +1,14 @@
+const head = document.getElementById("head")
 
 function acquireWallpaper(){
     
-    fetch("https://api.unsplash.com/photos/random?client_id=5r2vhIueAxpzUyn1CzUd-mtlH3kwz9oWUJ60xJn_-Wk&orientation=landscape&query=nature")
+    fetch("https://api.unsplash.com/photos/random?client_id=5r2vhIueAxpzUyn1CzUd-mtlH3kwz9oWUJ60xJn_-Wk&orientation=landscape&query=night")
         .then(res => res.json())
         .then(data => {
             document.body.style.backgroundImage = `url(${data.urls.full})`
-            const e = document.getElementById("head")
-            e.children[9].remove()
-            e.innerHTML += `<meta name="theme-color" content="${data.color}" />`
+            head.children[0].content = data.color
         })
 
 }
-
 
 export default acquireWallpaper
