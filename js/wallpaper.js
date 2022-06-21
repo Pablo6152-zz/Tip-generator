62f82c99fcd96141343e95cad94e6c075b03b0e0
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-06-21 18:22:28 -0500

    Squashed bug where wallpaper wouldn't appear

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -1,6 +1,5 @@
 
 function acquireWallpaper(){
-    appContainer.classList.add("test")
     fetch("https://api.unsplash.com/photos/random?client_id=5r2vhIueAxpzUyn1CzUd-mtlH3kwz9oWUJ60xJn_-Wk&orientation=landscape&query=nature")
         .then(res => res.json())
         .then(data => document.body.style.backgroundImage = `url(${data.urls.full})`)
