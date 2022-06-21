28d457f0c1c783eee19de5f15fb0c56c6874305a
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-06-21 17:23:46 -0500

    Improved calculation

--- js/index.js
+++ js/index.js
@@ -2,21 +2,33 @@ import * as counter from './counter.js'
 
 const totalAmountEl = document.getElementById("total-amount-el")
 const tipPercentageEl = document.getElementById("tip-percentage-el")
+const splitPersons = document.getElementById("split-persons")
 const appContainer = document.getElementById("site-bg")
 let finalTip = 0
+let eachPersonAmount = 0
 
 document.getElementById("calculate-btn").addEventListener("click", () =>{
     finalTip = totalAmountEl.value * (tipPercentageEl.value / 100)
 
+    if (splitPersons.value > 0){
+        eachPersonAmount = finalTip / splitPersons.value
+    }
+
     console.log (finalTip)
-    appContainer.innerHTML = ``
     appContainer.innerHTML = `
     
     <div id="main-component" class="after">
             <button id="go-back"><</button>
             <h2 class="operation-complete">Completed!</h2>
 
-            <p class="result-text">Paying <span class="accent-text">${totalAmountEl.value}</span> with <span class="accent-text">${tipPercentageEl.value}</span> of tip results in <span class="accent-text">${finalTip}</span></p>
+            <p class="result-text">
+                Paying <span class="accent-text">${totalAmountEl.value}$</span> 
+                with <span class="accent-text">${tipPercentageEl.value}%</span> of tip 
+                results in <span class="accent-text">${finalTip}$</span>,
+                ${eachPersonAmount}$ per person 😀.
+                </p>
+
+                
             
             <button id="share">!</button>
         </div>
